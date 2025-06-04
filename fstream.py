c1836053d36f018dc21efcf26220d84bb5826ddb
PIYUSH BIPIN VARULE <piyushvarule15@gmail.com>
2025-06-04 17:00:15 +0530

    Create fstream.py

---
+++ fstream.py
@@ -0,0 +1,7 @@
+# fstream methods in python
+info="hello my name is {} and i am {} years old"
+name="aakash"
+age="29"
+print(info.format(name,age))
+#another method
+print(f"hello my name is {name} and i am {age} years old")
