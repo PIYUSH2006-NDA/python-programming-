36db67367b56addfbed2074f2ce7077098b47686
PIYUSH BIPIN VARULE <piyushvarule15@gmail.com>
2025-06-07 01:42:46 +0530

    Create readwrite.py

---
+++ readwrite.py
@@ -0,0 +1,17 @@
+#with open("sample.txt","r") as open:
+#      t= open.read()
+#      print(t)
+#      open.close()
+# with open("sample.txt","w") as open:
+#      t= open.write("nice story")
+#      print(t)
+#      open.close()
+# with open("sample.txt","r") as open:
+#      while True:
+#           line=open.readline()
+#           print(line)
+#           if not line:
+#                print(line, type(line))
+#                break
+#           open.close()
+  
