89a631768296be8b9c9461c8cc6829dae4345fb0
PIYUSH BIPIN VARULE <piyushvarule15@gmail.com>
2025-06-07 01:32:44 +0530

    Create slash.py

---
+++ slash.py
@@ -0,0 +1,10 @@
+def welcome():
+     print("hello world")
+     if "__first____"=="__main__":
+         welcome()
+    
+#     code of first.py file 
+#     import main
+#     main.welcome()
+    
+#     output= hello world
